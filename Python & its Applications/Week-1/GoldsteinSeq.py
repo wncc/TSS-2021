ac8469a37420fdec763fc8f65be9593f965ceaaf
thevaliantthird <shubh5796@gmail.com>
2021-07-11 23:09:51 +0530

    Added Solutions for Week 1 Compulsory and Optional Assignments

---
+++ Python & its Applications/Week-1/GoldsteinSeq.py
@@ -0,0 +1,7 @@
+n = int(input('What would you want n to be?'))
+m = int(input('What would you want m to be?'))
+
+getInBase = (lambda m,n: m if m==0 else ((m%n) + (n+1)*(getInBase(m//n,n))))
+GoodStein = lambda m,n: m if n==1 else getInBase(GoodStein(m,n-1),n)
+
+print(GoodStein(m,n))
