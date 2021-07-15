0c32ec3fafb726da7a633ee92364a5eceb37f57f
Karrthik Arya <76391390+Karrthik-Arya@users.noreply.github.com>
2021-07-15 19:45:52 +0530

    Delete GoldsteinSeq.py

--- Python & its Applications/Week-1/GoldsteinSeq.py
+++
@@ -1,9 +0,0 @@
-#Point the conciseness and compactness with which you can define functions in Python!
-
-n = int(input('What would you want n to be?'))
-m = int(input('What would you want m to be?'))
-
-getInBase = (lambda m,n: m if m==0 else ((m%n) + (n+1)*(getInBase(m//n,n))))
-GoodStein = lambda m,n: m if n==1 else getInBase(GoodStein(m,n-1),n)
-
-print(GoodStein(m,n))
